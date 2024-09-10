6af30d1a62b37644a067874d0a1493df38429301
Halim-Farazi <halimcse17@gmail.com>
2024-09-10 21:53:53 +0600

    add all

--- index.js
+++ index.js
@@ -4,34 +4,39 @@ const team2Name = document.querySelector("#t2input");
 const t1elm = document.querySelector(".t1");
 const t2elm = document.querySelector(".t2");
 const winner = document.querySelector("#result");
+const resetBtnElm = document.querySelector("#resetBtn");
 
-/*
 function validateValue(val1, val2) {
-  if (val1.trim === "" || typeof val1 !== "string") {
-    alert("input a valid number");
-    return false;
-  } else if (val2.trim == "" || typeof val2 !== "string") {
+  if (val1.trim === "" && val2.trim == "") {
     alert("input a valid number");
     return false;
   } else {
     return true;
   }
 }
-*/
 
 function tossWinner(T1, T2) {
-  const teams = [T1, T2];
-  const index = Math.floor(Math.random() * teams.length);
-  const team = teams[index];
-  console.log(team);
-  return team;
+  return Math.random() > 0.5 ? T1 : T2;
 }
+
+function resetValue() {
+  T1Name = "";
+  T2Name = "";
+  t1elm.textContent = "";
+  t2elm.textContent = "";
+  winner.textContent = "";
+}
+
+resetBtnElm.addEventListener("click", (evt) => {
+  resetValue();
+});
 
 formElm.addEventListener("submit", (evt) => {
   evt.preventDefault();
+  resetValue();
   const T1Name = team1Name.value;
   const T2Name = team2Name.value;
-  validateValue(T1Name, T2Name);
+  if (!validateValue(T1Name, T2Name)) return;
   t1elm.textContent = T1Name;
   t2elm.textContent = T2Name;
   let tossWinningTeam = tossWinner(T1Name, T2Name);
